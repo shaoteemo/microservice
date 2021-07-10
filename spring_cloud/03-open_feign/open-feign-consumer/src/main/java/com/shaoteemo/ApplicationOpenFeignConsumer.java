a73f68da00733e11c8c3801f9d1e2068c93617a7
ShaoTeemo <shaoteemo@qq.com>
2021-07-10 18:35:03 +0800

    up

--- spring_cloud/03-open_feign/open-feign-consumer/src/main/java/com/shaoteemo/ApplicationOpenFeignConsumer.java
+++ spring_cloud/03-open_feign/open-feign-consumer/src/main/java/com/shaoteemo/ApplicationOpenFeignConsumer.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 /*
- * 知名当前服务为使用OpenFeign技术开发的服务端
+ * 指明当前服务为使用OpenFeign技术开发的服务端
  * 属性：basePackages表明@FeignClient接口所在的包，可以忽略默认扫描项目所有包
  */
 @EnableFeignClients(basePackages = "com.shaoteemo.service")
